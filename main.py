96b77ac9e8686e09890557e83bb8b5898ebbeb33
humansprite <humansprite@gmail.com>
2017-01-01 00:20:04 +0800

    单列符合类型，表达式类型

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import inspect
+import re
 import xlrd
 
 # 定义符号
@@ -8,6 +9,7 @@ type_dict = 'dict'
 type_int = 'int'
 type_number = 'number'
 type_string = 'string'
+type_expr = 'expr'
 
 # 定义简单类型
 simple_type_def = {
@@ -27,6 +29,10 @@ complex_token_def = {
 def is_simple_type(ty):
     return ty in simple_type_def
 
+def is_single_col_complex_type(ty):
+    for k,v in complex_token_def.items():
+        if ty == v[0] + v[1]:
+            return k
 
 # 将整数转换为excel的列
 def to_xls_col(num):
@@ -37,10 +43,10 @@ def to_xls_col(num):
         factor = tmp / 26
         remain = tmp % 26
         tmp = factor
-        str += chr(remain + A)
+        str = chr(remain + A) + str
         if factor == 0:
             break
-    return str[::-1]
+    return str
 
 
 # 读取xls文件内容，并过滤注释行
@@ -91,11 +97,18 @@ class Proto:
                 return value
         return None
 
+    def colstr(self):
+        if self.begin_col == self.end_col:
+            return '(col %s)' % to_xls_col(self.begin_col)
+        else:
+            return '(col %s,%s)' % (to_xls_col(self.begin_col), to_xls_col(self.end_col))
+
     # 打印输出
     def tostr(self, identity):
         result = ''
         if self.type == type_array:
-            result += '%s (col %s,%s)' % (self.type, to_xls_col(self.begin_col), to_xls_col(self.end_col))
+            result += self.type
+            result += self.colstr()
             result += '[\n'
             for i in range(len(self.members)):
                 (_, pt) = self.members[i]
@@ -106,16 +119,14 @@ class Proto:
             result += '\t' * identity
             result += ']'
         elif self.type == type_dict:
-            result += '%s (col %s,%s)' % (self.type, to_xls_col(self.begin_col), to_xls_col(self.end_col))
+            result += self.type
+            result += self.colstr()
             result += '{\n'
             for i in range(len(self.members)):
                 (name, pt) = self.members[i]
                 result += '\t' * (identity+1)
                 result += name
-                if pt.begin_col == pt.end_col:
-                    result += '(col %s)' % to_xls_col(pt.begin_col)
-                else:
-                    result += '(col %s,%s)' % (to_xls_col(pt.begin_col), to_xls_col(pt.end_col))
+                result += pt.colstr()
                 result += ': '
                 result += pt.tostr(identity+1)
                 result += ','
@@ -123,11 +134,32 @@ class Proto:
             result += '\t' * identity
             result += '}'
         else:
-            result = '%s(col %s)' % (self.type, to_xls_col(self.begin_col))
+            result += self.type
+            result += self.colstr()
         return result
 
     def __str__(self):
         return self.tostr(0)
+
+
+class Expr(Proto):
+    def __init__(self):
+        Proto.__init__(self, type_expr)
+        self.argument = []
+
+    def add_argment(self, arg):
+        self.argument.append(arg)
+
+    def tostr(self, identity):
+        result = 'expr('
+        arg_count = len(self.argument)
+        for i in range(arg_count):
+            arg = self.argument[i]
+            result += arg
+            if i < arg_count - 1:
+                result += ','
+        result += ')'
+        return result + self.colstr()
 
 
 # 解析器
@@ -135,10 +167,18 @@ class Parser:
     def __init__(self, ts):
         self.tokens = ts
         self.pos = 0
-        self.proto_node = Proto(type_dict)
-        self.proto_node.begin_col = self.cur_token().col
-        self.parse_dict(self.proto_node)
-        self.proto_node.end_col = self.tokens[self.pos-1].col
+
+        # 配置符合类型对应的解析函数
+        self.parser_cfg = {
+            type_array: self.parse_array,
+            type_dict: self.parse_dict,
+        }
+
+        root_node = Proto(type_dict)
+        root_node.begin_col = self.cur_token().col
+        self.parse_dict(root_node)
+        root_node.end_col = self.tokens[self.pos-1].col
+        self.proto_node = root_node
 
     # 取得当前读取位置的符号
     def cur_token(self):
@@ -170,21 +210,30 @@ class Parser:
     # 解析复合类型
     def parse_complex_value(self, pt):
         cur = self.cur_token()
-        if cur.ty == complex_token_def[type_array][0]:
-            value = Proto(type_array)
-            value.begin_col = cur.col
-            self.skip_token()
-            self.parse_array(value)
-            return value
-        elif cur.ty == complex_token_def[type_dict][0]:
-            value = Proto(type_dict)
-            value.begin_col = cur.col
-            self.skip_token()
-            self.parse_dict(value)
-            return value
-        else:
-            self.parse_error('解析列%d的%s类型元素时,遇到了位于列%d的意外的符号\'%s\'' % (pt.begin_col, pt.type, cur.col, str(cur)))
-            return None
+        for k,v in complex_token_def.items():
+            if v[0] == cur.ty:
+                value = Proto(type_array)
+                value.begin_col = cur.col
+                self.skip_token()
+                self.parser_cfg[k](value)
+                return value
+        self.parse_error('解析列%d的%s类型元素时,遇到了位于列%d的意外的符号\'%s\'' % (pt.begin_col, pt.type, cur.col, str(cur)))
+        return None
+
+    # 解析表达式类型
+    def parse_expr(self):
+        cur = self.cur_token()
+        pt = Expr()
+        pt.begin_col = cur.col
+        pt.end_col = cur.col
+        ty = cur.ty
+        if ty != type_expr:
+            m = re.match('expr\((.+)\)', ty)
+            if m is not None:
+                arg_str = m.group(1)
+                for arg in arg_str.split(','):
+                    pt.add_argment(arg)
+        return pt
 
     # 解析数组类型
     def parse_array(self, arr):
@@ -203,12 +252,20 @@ class Parser:
                 value = self.parse_simple_value()
                 self.skip_token()
             else:
-                value = self.parse_complex_value(arr)
+                # 解析表达式类型
+                if cur.ty.startswith(type_expr):
+                    value = self.parse_expr()
+                else:
+                    fold_ty = is_single_col_complex_type(cur.ty)
+                    if fold_ty is not None:
+                        value = Proto(fold_ty)
+                        value.begin_col = cur.col
+                        value.end_col = cur.col
+                    else:
+                        value = self.parse_complex_value(dict)
                 self.skip_token()
-                if value is None:
-                    break
-
-            arr.add_member(None, value)
+            if value is not None:
+                arr.add_member(None, value)
             cur = self.cur_token()
 
     # 解析字典类型
@@ -233,25 +290,33 @@ class Parser:
                 value = self.parse_simple_value()
                 self.skip_token()
             else:
-                value = self.parse_complex_value(dict)
+                # 解析表达式类型
+                if cur.ty.startswith(type_expr):
+                    value = self.parse_expr()
+                else:
+                    fold_ty = is_single_col_complex_type(cur.ty)
+                    if fold_ty is not None:
+                        value = Proto(fold_ty)
+                        value.begin_col = cur.col
+                        value.end_col = cur.col
+                    else:
+                        value = self.parse_complex_value(dict)
                 self.skip_token()
-                if value is None:
-                    break
-
-            # 路径变量需要解析取得操作对象
-            idpath = cur.id.split('.')
-            length = len(idpath)
-            if length > 1:
-                last_pt = dict
-                for i in range(length-1):
-                    key = idpath[i]
-                    last_pt = last_pt.get_member(key)
-                    if last_pt is None:
-                        self.parse_error('idpath(%s) is invalid' % cur.id)
-                        break
-                last_pt.add_member(idpath[length-1], value)
-            else:
-                dict.add_member(cur.id, value)
+            if value is not None:
+                # 路径变量需要解析取得操作对象
+                idpath = cur.id.split('.')
+                length = len(idpath)
+                if length > 1:
+                    last_pt = dict
+                    for i in range(length-1):
+                        key = idpath[i]
+                        last_pt = last_pt.get_member(key)
+                        if last_pt is None:
+                            self.parse_error('idpath(%s) is invalid' % cur.id)
+                            break
+                    last_pt.add_member(idpath[length-1], value)
+                else:
+                    dict.add_member(cur.id, value)
             cur = self.cur_token()
 
 if __name__ == "__main__":
